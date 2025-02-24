6e6939dce3c04e55798466539c06f994d35768af
jjbmsda <jjbmsda@naver.com>
2025-02-23 21:34:01 -0800

    update twosum

--- twosum.py
+++ twosum.py
@@ -5,9 +5,9 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        output = []
-        for i in range (len(nums)):
-            for j in range (i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    output = [i, j]
-                    return output
+        num_map = {}  # 값 -> 인덱스를 저장할 딕셔너리
+        for i, num in enumerate(nums):
+            complement = target - num  # 필요한 값 (target - 현재 값)
+            if complement in num_map:  # 이미 저장된 값이면 정답
+                return [num_map[complement], i]
+            num_map[num] = i  # 현재 숫자를 딕셔너리에 추가
