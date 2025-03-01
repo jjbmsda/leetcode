4a48a362e2b0ba68bd3a77eb184544fff359e0f4
Joseph Jungbu Jang <57055431+jjbmsda@users.noreply.github.com>
2025-02-28 20:45:04 -0800

    Update twosum.py

--- twosum.py
+++ twosum.py
@@ -10,4 +10,4 @@ class Solution(object):
             complement = target - num  # 필요한 값 (target - 현재 값)
             if complement in num_map:  # 이미 저장된 값이면 정답
                 return [num_map[complement], i]
-            num_map[num] = i  # 현재 숫자를 딕셔너리에 추가
+            num_map[num] = i  # 현재 숫자를 딕셔너리에 추가함
